20fee7584586144d12f25bc0156e67d721109d09
zhanghong <kh2335735@gmail.com>
2025-12-24 14:20:22 +0800

    新增缓存菜品代码开发

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -10,9 +10,11 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/admin/dish")
@@ -22,6 +24,9 @@ public class DishController {
 
     @Autowired
     private DishService dishService;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     /**
      * 新增菜品
@@ -33,6 +38,11 @@ public class DishController {
     public Result save(@RequestBody DishDTO dishDTO){
         log.info("新增菜品：{}", dishDTO);
         dishService.saveWithFlavor(dishDTO);
+
+        //新增菜品后需要删除缓存中该分类下的菜品数据，后续实现
+        String key = "DISH_" + dishDTO.getCategoryId();
+        clearCache(key);
+
         return Result.success();
     }
 
@@ -59,6 +69,11 @@ public class DishController {
     public Result delete(@RequestParam List<Long> ids) {
         log.info("菜品批量删除：{}", ids);
         dishService.deleteBatch(ids);//后绪步骤实现
+
+        //删除菜品后需要删除缓存中该分类下的菜品数据
+        //由于可能删除多个菜品，且这些菜品可能属于不同分类，因此需要把所有缓存删除
+        clearCache("DISH_*");
+
         return Result.success();
     }
 
@@ -88,6 +103,10 @@ public class DishController {
     public Result update(@RequestBody DishDTO dishDTO) {
         log.info("修改菜品：{}", dishDTO);
         dishService.updateWithFlavor(dishDTO);
+
+        //修改菜品后需要删除缓存中该分类下的菜品数据
+        clearCache("DISH_*");
+
         return Result.success();
     }
 
@@ -96,6 +115,10 @@ public class DishController {
     public Result<String> changeStatus(@PathVariable Integer status, @RequestParam Long id){
         log.info("起售/停售菜品：{}, {}", status, id);
         dishService.changeStatus(status, id);
+
+        //修改菜品后需要删除缓存中该分类下的菜品数据
+        clearCache("DISH_*");
+
         return Result.success();
     }
 
@@ -109,5 +132,14 @@ public class DishController {
     public Result<List<DishVO>> list(Long categoryId){
         List<DishVO> list = dishService.list(categoryId);
         return Result.success(list);
+    }
+
+    /**
+     * 清除缓存
+     * @param keyPattern
+     */
+    private void clearCache(String keyPattern) {
+        Set keys = redisTemplate.keys(keyPattern);
+        redisTemplate.delete(keys);
     }
 }
