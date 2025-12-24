c94dd297a5ada16a74d06f3a53b53b76e883d9e3
zhanghong <kh2335735@gmail.com>
2025-12-24 21:35:09 +0800

    新增购物车业务代码

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -10,6 +10,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,6 +26,7 @@ public class SetmealController {
 
     @PostMapping
     @ApiOperation("新增套餐")
+    @CacheEvict(cacheNames = "setmealCache",key = "#setmealDTO.categoryId")//key: setmealCache::100
     public Result save(@RequestBody SetmealDTO setmealDTO){
         log.info("新增套餐：{}", setmealDTO);
         setmealService.addSetmeal(setmealDTO);
@@ -43,12 +45,13 @@ public class SetmealController {
     @ApiOperation("根据id查询套餐信息")
     public Result<SetmealVO> getById(@PathVariable Long id) {
         log.info("根据id查询套餐信息：{}", id);
-        SetmealVO SetmealVO = setmealService.getByIdWithSetmealDish(id);
-        return Result.success(SetmealVO);
+        SetmealVO setmealVO = setmealService.getByIdWithSetmealDish(id);
+        return Result.success(setmealVO);
     }
 
     @DeleteMapping
     @ApiOperation("批量删除套餐")
+    @CacheEvict(cacheNames = "setmealCache", allEntries = true)
     public Result delete(@RequestParam List<Long> ids) {
         log.info("套餐批量删除：{}", ids);
         setmealService.deteleWithSetmealDish(ids);
@@ -57,6 +60,7 @@ public class SetmealController {
 
     @PutMapping
     @ApiOperation("修改套餐信息")
+    @CacheEvict(cacheNames = "setmealCache", allEntries = true)
     public Result update(@RequestBody SetmealDTO setmealDTO){
         log.info("修改套餐信息：{}", setmealDTO);
         setmealService.update(setmealDTO);
@@ -65,6 +69,7 @@ public class SetmealController {
 
     @PostMapping("/status/{status}")
     @ApiOperation("起售/停售套餐")
+    @CacheEvict(cacheNames = "setmealCache", allEntries = true)
     public Result updateStatus(@PathVariable Integer status, Long id){
         log.info("起售/停售套餐：{},{}", status,id);
         setmealService.updateStatus(status,id);
---
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -0,0 +1,76 @@
+package com.sky.controller.user;
+
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
+import com.sky.result.Result;
+import com.sky.service.ShoppingCartService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user/shoppingCart")
+@Slf4j
+@Api(tags = "C端购物车接口")
+public class ShoppingCartController {
+
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
+    /**
+     * 添加购物车
+     * @param shoppingCartDTO
+     * @return
+     */
+    @ApiOperation("添加购物车")
+    @PostMapping("/add")
+    public Result<String> add(@RequestBody ShoppingCartDTO shoppingCartDTO){
+        log.info("添加购物车：{}",shoppingCartDTO);
+        shoppingCartService.addShoppingCart(shoppingCartDTO);
+        return Result.success();
+    }
+
+    /**
+     * 删除购物车中的一个商品
+     * @param shoppingCartDTO
+     * @return
+     */
+    @ApiOperation("删除购物车中的一个商品")
+    @PostMapping("/sub")
+    public Result<String> sub(@RequestBody ShoppingCartDTO shoppingCartDTO){
+        log.info("添加购物车：{}",shoppingCartDTO);
+        shoppingCartService.subShoppingCart(shoppingCartDTO);
+        return Result.success();
+    }
+
+    /**
+     * 查看购物车
+     * @param
+     * @return
+     */
+    @ApiOperation("查看购物车")
+    @GetMapping("/list")
+    public Result<List<ShoppingCart>> list(){
+        log.info("查看购物车：");
+        List<ShoppingCart> shoppingCart = shoppingCartService.list();
+        return Result.success(shoppingCart);
+    }
+
+    /**
+     * 清空购物车
+     * @param
+     * @return
+     */
+    @ApiOperation("查看购物车")
+    @DeleteMapping("/clean")
+    public Result clean(){
+        log.info("清空购物车");
+        shoppingCartService.clean();
+        return Result.success();
+    }
+
+}
